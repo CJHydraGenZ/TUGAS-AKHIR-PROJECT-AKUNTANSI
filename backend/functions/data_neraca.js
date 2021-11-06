e18384aaa6bdc4023c77e368e72042942a29b983
Cahya <cahyasaga321@gmail.com>
2021-11-06 10:29:36 +0800

    get persediaaan

--- backend/functions/data_neraca.js
+++ backend/functions/data_neraca.js
@@ -1,98 +1,160 @@
-exports.data_neraca = (data) => {
-  const al = data
+const wraping = (e, t, data) => {
+  const res = data;
+  const ename = res
     .map((x, i) => {
-      if (x.funcL === "al") {
+      if (x.funcL === e) {
         return x;
       }
     })
     .filter((f) => f);
-  let totalAL = al
+  let total = ename
     .map(({ item, jumlahHarga }) => +jumlahHarga)
     .reduce((acc, curr) => curr + acc, 0);
 
-  const atl = data
-    .map((x, i) => {
-      if (x.funcL === "atl") {
-        return x;
-      }
-    })
-    .filter((f) => f);
-  let totalATL = atl
-    .map(({ item, jumlahHarga }) => +jumlahHarga)
-    .reduce((acc, curr) => curr + acc, 0);
+  return {
+    [e]: ename,
+    [t]: total,
+  };
+};
 
-  const at = data
-    .map((x, i) => {
-      if (x.funcL === "at") {
-        return x;
-      }
-    })
-    .filter((f) => f);
-  let totalAT = at
-    .map(({ item, jumlahHarga }) => +jumlahHarga)
-    .reduce((acc, curr) => curr - acc, 0);
-  const kl = data
-    .map((x, i) => {
-      if (x.funcL === "kl") {
-        return x;
-      }
-    })
-    .filter((f) => f);
-  let totalKL = kl
-    .map(({ item, jumlahHarga }) => +jumlahHarga)
-    .reduce((acc, curr) => curr + acc, 0);
+exports.data_laba_rugi = (data) => {
+  //? laba rugi
+  const { tpu, totalTPU } = wraping("tpu", "totalTPU", data);
+  const { tbp, totalTBP } = wraping("tbp", "totalTBP", data);
+  const { bpeg, totalBPEG } = wraping("bpeg", "totalBPEG", data);
+  const { bk, totalBK } = wraping("bk", "totalBK", data);
+  const { bpen, totalBPEN } = wraping("bpen", "totalBPEN", data);
+  const { pl, totalPL } = wraping("pl", "totalBL", data);
+  const { bl, totalBL } = wraping("bl", "totalBL", data);
+  // console.log("ini res", tpu);
+  //? neraca
+  const { al, totalAL } = wraping("al", "totalAL", data); //!  untuk aset tetap rumusnya beda
+  const { atl, totalATL } = wraping("atl", "totalATL", data);
+  const { at, totalAT } = wraping("at", "totalAT", data);
+  const { kl, totalKL } = wraping("kl", "totalKL", data);
+  const { kjp, totalKJP } = wraping("kjp", "totalKJP", data);
+  const { ekuitas, totalEKUITAS } = wraping("ekuitas", "totalEKUITAS", data);
 
-  const kjp = data
-    .map((x, i) => {
-      if (x.funcL === "kjp") {
-        return x;
-      }
-    })
-    .filter((f) => f);
-  let totalKJP = kjp
-    .map(({ item, jumlahHarga }) => +jumlahHarga)
-    .reduce((acc, curr) => curr + acc, 0);
-  const ekuitas = data
-    .map((x, i) => {
-      if (x.funcL === "ekuitas") {
-        return x;
-      }
-    })
-    .filter((f) => f);
-  let totalEKUITAS = ekuitas
-    .map(({ item, jumlahHarga }) => +jumlahHarga)
-    .reduce((acc, curr) => curr + acc, 0);
-
+  //? lap. keuangan
+  const { kdsk, totalKDSK } = wraping("kdsk", "totalKDSK", data);
+  const { pd, totalPD } = wraping("pd", "totalPD", data);
+  const { pil, totalPIL } = wraping("pil", "totalPIL", data);
+  const { per, totalPER } = wraping("per", "totalPER", data);
+  const { um, totalUM } = wraping("um", "totalUM", data);
+  const { uu, totalUU } = wraping("uu", "totalUU", data);
+  const { dt, totalDT } = wraping("dt", "totalDT", data);
+  const { ul, totalUL } = wraping("ul", "totalUL", data);
+  const { up, totalUP } = wraping("up", "totalUP", data);
+  //! rumus
   let TOTAL_JUMLAH_ASET = totalAL + totalATL + totalAT;
   let TOTAL_KNE = totalKL + totalKJP + totalEKUITAS;
-  const NData = {
-    al: {
-      data: al,
-      total: totalAL,
-    },
-    atl: {
-      data: atl,
-      total: totalATL,
-    },
-    at: {
-      data: at,
-      total: totalAT,
-    },
-    kl: {
-      data: kl,
-      total: totalKL,
-    },
-    kjp: {
-      data: kjp,
-      total: totalKJP,
-    },
-    ekuitas: {
-      data: ekuitas,
-      total: totalEKUITAS,
-    },
 
-    TOTAL_JUMLAH_ASET,
-    TOTAL_KNE,
+  let LR1 = totalTPU - totalTBP;
+  let LR2 = totalBPEG + totalBK + totalBPEN;
+  let LR3 = totalBL - totalPL;
+  let LABA_RUGI = LR1 - LR2 + LR3;
+  const LData = {
+    laba_rugi: {
+      tpu: {
+        data: tpu,
+        total: totalTPU,
+      },
+      tbp: {
+        data: tbp,
+        total: totalTBP,
+      },
+      bpeg: {
+        data: bpeg,
+        total: totalBPEG,
+      },
+      bk: {
+        data: bk,
+        total: totalBK,
+      },
+      bpen: {
+        data: bpen,
+        total: totalBPEN,
+      },
+      pl: {
+        data: pl,
+        total: totalPL,
+      },
+      bl: {
+        data: bl,
+        total: totalBL,
+      },
+      LR1: LR1,
+      LR2: LR2,
+      LR3: LR3,
+      LABA_RUGI: LABA_RUGI,
+    },
+    neraca: {
+      al: {
+        data: al,
+        total: totalAL,
+      },
+      atl: {
+        data: atl,
+        total: totalATL,
+      },
+      at: {
+        data: at,
+        total: totalAT,
+      },
+      kl: {
+        data: kl,
+        total: totalKL,
+      },
+      kjp: {
+        data: kjp,
+        total: totalKJP,
+      },
+      ekuitas: {
+        data: ekuitas,
+        total: totalEKUITAS,
+      },
+
+      TOTAL_JUMLAH_ASET,
+      TOTAL_KNE,
+    },
+    lap_keuangan: {
+      kdsk: {
+        data: kdsk,
+        total: totalKDSK,
+      },
+      pd: {
+        data: pd,
+        total: totalPD,
+      },
+      pil: {
+        data: pil,
+        total: totalPIL,
+      },
+      per: {
+        data: per,
+        total: totalPER,
+      },
+      um: {
+        data: um,
+        total: totalUM,
+      },
+      uu: {
+        data: uu,
+        total: totalUU,
+      },
+      dt: {
+        data: dt,
+        total: totalDT,
+      },
+      ul: {
+        data: ul,
+        total: totalUL,
+      },
+      up: {
+        data: up,
+        total: totalUP,
+      },
+    },
   };
-  return NData;
+  return LData;
 };
